f7f2d610ca02f41befe535e886d83e2296400e99
Marcelo Lima <m-lima@users.noreply.github.com>
2022-11-26 15:06:15 +0100

    Reorg colors

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,56 @@
-macro_rules! color {
+macro_rules! style {
     (reset) => {
         "[m"
     };
 
-    (fg black) => {
-        "[30m"
-    };
-    (fg red) => {
-        "[31m"
-    };
-    (fg green) => {
-        "[32m"
-    };
-    (fg yellow) => {
-        "[33m"
-    };
-    (fg blue) => {
-        "[34m"
-    };
-    (fg magenta) => {
-        "[35m"
-    };
-    (fg cyan) => {
-        "[36m"
-    };
-    (fg white) => {
-        "[37m"
-    };
-    (fg [$param: literal]) => {
-        concat!("[38;5;", $param, "m")
-    };
-    (fg [$r: literal, $g: literal, $b: literal]) => {
-        concat!("[38;2;", $r, ";", $g, ";", $b, "m")
-    };
-    (fg reset) => {
-        "[39m"
+    (fg = $color: expr $(, $($param: expr)*)?) => {
+        concat!("[3", $color, "m" $(, $($param),*)?)
     };
 
-    (bg black) => {
-        "[40m"
+    (bg = $color: expr $(, $($param: expr)*)?) => {
+        concat!("[4", $color, "m" $(, $($param),*)?)
     };
-    (bg red) => {
-        "[41m"
-    };
-    (bg green) => {
-        "[42m"
-    };
-    (bg yellow) => {
-        "[43m"
-    };
-    (bg blue) => {
-        "[44m"
-    };
-    (bg magenta) => {
-        "[45m"
-    };
-    (bg cyan) => {
-        "[46m"
-    };
-    (bg white) => {
-        "[47m"
-    };
-    (bg [$param: literal]) => {
-        concat!("[48;5;", $param, "m")
-    };
-    (bg [$r: literal, $g: literal, $b: literal]) => {
-        concat!("[48;2;", $r, ";", $g, ";", $b, "m")
-    };
-    (bg reset) => {
-        "[49m"
+
+    (fg = $fg: expr, bg = $bg: expr $(, $($param: expr)*)?) => {
+        concat!("[3", $fg, "m", "[4", $bg, "m" $(, $($param),*)?)
     };
 }
 
+macro_rules! color {
+    (black) => {
+        0
+    };
+    (red) => {
+        1
+    };
+    (green) => {
+        2
+    };
+    (yellow) => {
+        3
+    };
+    (blue) => {
+        4
+    };
+    (magenta) => {
+        5
+    };
+    (cyan) => {
+        6
+    };
+    (white) => {
+        7
+    };
+    ([$param: literal]) => {
+        concat!("8;5;", $param)
+    };
+    ([$r: literal, $g: literal, $b: literal]) => {
+        concat!("8;2;", $r, ";", $g, ";", $b)
+    };
+    (reset) => {
+        9
+    };
+}
 macro_rules! symbol {
     (error) => {
         "✘"
@@ -96,37 +75,23 @@ macro_rules! symbol {
     };
 }
 
-fn pwd(path: Option<String>) -> String {
-    path.map_or_else(
-        || {
-            println!(concat!(
-                color!(fg red),
-                "`PWD` environment variable not available",
-                color!(reset)
-            ));
-            String::new()
-        },
-        |pwd| {
-            if let Ok(home) = std::env::var("HOME") {
-                if pwd == home {
-                    String::from("~")
-                } else {
-                    let mut parts = pwd.split(std::path::MAIN_SEPARATOR);
-                    let head = parts.next();
-                    let tail = parts.last().filter(|t| !t.is_empty());
-                    tail.or_else(|| head.filter(|h| !h.is_empty()))
-                        .map_or_else(|| String::from(std::path::MAIN_SEPARATOR), String::from)
-                }
-            } else {
-                println!(concat!(
-                    color!(fg red),
-                    "`HOME` environment variable not available",
-                    color!(reset)
-                ));
-                pwd
-            }
-        },
-    )
+fn pwd(path: String) -> String {
+    if let Ok(home) = std::env::var("HOME") {
+        if path == home {
+            String::from("~")
+        } else {
+            let mut parts = path.split(std::path::MAIN_SEPARATOR);
+            let head = parts.next();
+            let tail = parts.last().filter(|t| !t.is_empty());
+            tail.or_else(|| head.filter(|h| !h.is_empty()))
+                .map_or_else(|| String::from(std::path::MAIN_SEPARATOR), String::from)
+        }
+    } else {
+        println!(
+            style!(fg = color!(red), "`HOME` environment variable not available" style!(reset))
+        );
+        path
+    }
 }
 
 fn left(host: impl std::fmt::Display, args: impl Iterator<Item = String>) {
@@ -141,45 +106,37 @@ fn left(host: impl std::fmt::Display, args: impl Iterator<Item = String>) {
     });
 
     let error = if error {
-        concat!(color!(fg red), symbol!(error), " ")
+        style!(fg = color!(red), symbol!(error) " ")
     } else {
         ""
     };
 
     let jobs = if jobs {
-        concat!(color!(fg cyan), symbol!(jobs), " ")
+        style!(fg = color!(cyan), symbol!(jobs) " ")
     } else {
         ""
     };
 
     let venv = if std::env::var("VIRTUAL_ENV").is_ok() {
-        concat!(color!(fg green), " ") // ")
+        style!(fg = color!(green), " ") // ")
     } else {
         ""
     };
 
     print!(
         concat!(
-            color!(bg black),
-            " {error}{jobs}{venv}",
-            color!(reset),
-            color!(bg black),
-            "{host}",
-            color!(reset),
-            color!(bg black),
-            " {pwd} ",
-            color!(fg black),
-            color!(bg blue),
-            symbol!(div),
-            color!(fg blue),
-            color!(bg reset),
-            symbol!(div),
+            style!(bg = color!(black), " {error}{jobs}{venv}"),
+            style!(fg = color!(reset), "{host}"),
+            style!(reset),
+            style!(bg = color!(black), " {pwd} "),
+            style!(fg = color!(black), bg = color!(blue), symbol!(div)),
+            style!(fg = color!(blue), bg = color!(reset), symbol!(div)),
         ),
         error = error,
         jobs = jobs,
         venv = venv,
         host = host,
-        pwd = pwd(std::env::var("PWD").ok()),
+        pwd = pwd(std::env::var("PWD").unwrap()),
     );
 }
 
@@ -188,7 +145,7 @@ fn right() {
 
     let time = chrono::DateTime::<chrono::Local>::from(std::time::SystemTime::now());
     print!(
-        concat!(color!(fg[23]), "{h:02}:{m:02}:{s:02}", color!(reset)),
+        style!(fg = color!([23]), "{h:02}:{m:02}:{s:02}" style!(reset)),
         h = time.hour(),
         m = time.minute(),
         s = time.second(),
@@ -249,7 +206,7 @@ mod tests {
         ]
         .map(|(a, b)| {
             (
-                Some(a.replace('/', String::from(std::path::MAIN_SEPARATOR).as_str())),
+                a.replace('/', String::from(std::path::MAIN_SEPARATOR).as_str()),
                 b.replace('/', String::from(std::path::MAIN_SEPARATOR).as_str()),
             )
         });
