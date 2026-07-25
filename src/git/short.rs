e06730b27e2a02fcc3408727bbdb2046b71880f0
Marcelo Lima <m-lima@users.noreply.github.com>
2026-07-25 09:07:33 +0200

    Add git tests (short)

--- src/git/short.rs
+++ src/git/short.rs
@@ -18,7 +18,7 @@ pub enum Sync {
     Local,
 }
 
-pub fn parse(path: &std::path::PathBuf) -> Repo {
+pub fn parse(path: &std::path::Path) -> Repo {
     let Some(repo) = git2::Repository::discover(path).ok() else {
         return Repo::None;
     };
@@ -85,4 +85,245 @@ fn get_sync(
         (false, true) => Sync::Ahead,
         (true, true) => Sync::Diverged,
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn none() {
+        let dir = tempfile::tempdir().unwrap();
+        let path = dir.path();
+        assert_eq!(Repo::None, parse(path));
+    }
+
+    mod sync {
+        use super::super::*;
+
+        #[test]
+        fn behind() {
+            let dir = tempfile::tempdir().unwrap();
+            let path = dir.path();
+            let repo = git2::Repository::init(path).unwrap();
+            repo.remote("origin", "file:///dev/null").unwrap();
+            let sig = repo.signature().unwrap();
+
+            let mut idx = repo.index().unwrap();
+            let id = idx.write_tree().unwrap();
+            let tree = repo.find_tree(id).unwrap();
+            let oid = repo
+                .commit(Some("HEAD"), &sig, &sig, "initial commit", &tree, &[])
+                .unwrap();
+
+            repo.reference("refs/remotes/origin/master", oid, true, "remote")
+                .unwrap();
+            let mut local_branch = repo.find_branch("master", git2::BranchType::Local).unwrap();
+            local_branch.set_upstream(Some("origin/master")).unwrap();
+
+            let commit_oid = repo.find_commit(oid).unwrap();
+            let new_oid = repo
+                .commit(
+                    Some("HEAD"),
+                    &sig,
+                    &sig,
+                    "second commit",
+                    &tree,
+                    &[&commit_oid],
+                )
+                .unwrap();
+
+            let oid_obj = repo.find_object(oid, None).unwrap();
+            repo.reset(&oid_obj, git2::ResetType::Hard, None).unwrap();
+            repo.reference("refs/remotes/origin/master", new_oid, true, "remote")
+                .unwrap();
+
+            assert_eq!(Repo::Clean(Sync::Behind), parse(path));
+            std::fs::write(path.join("yo"), b"yo").unwrap();
+            assert_eq!(Repo::Dirty(Sync::Behind), parse(path));
+        }
+
+        #[test]
+        fn ahead() {
+            let dir = tempfile::tempdir().unwrap();
+            let path = dir.path();
+            let repo = git2::Repository::init(path).unwrap();
+            repo.remote("origin", "file:///dev/null").unwrap();
+            let sig = repo.signature().unwrap();
+
+            let mut idx = repo.index().unwrap();
+            let id = idx.write_tree().unwrap();
+            let tree = repo.find_tree(id).unwrap();
+            let oid = repo
+                .commit(Some("HEAD"), &sig, &sig, "initial commit", &tree, &[])
+                .unwrap();
+
+            repo.reference("refs/remotes/origin/master", oid, true, "remote")
+                .unwrap();
+            let mut local_branch = repo.find_branch("master", git2::BranchType::Local).unwrap();
+            local_branch.set_upstream(Some("origin/master")).unwrap();
+
+            let commit_oid = repo.find_commit(oid).unwrap();
+            repo.commit(
+                Some("HEAD"),
+                &sig,
+                &sig,
+                "second commit",
+                &tree,
+                &[&commit_oid],
+            )
+            .unwrap();
+
+            assert_eq!(Repo::Clean(Sync::Ahead), parse(path));
+            std::fs::write(path.join("yo"), b"yo").unwrap();
+            assert_eq!(Repo::Dirty(Sync::Ahead), parse(path));
+        }
+
+        #[test]
+        fn diverged() {
+            let dir = tempfile::tempdir().unwrap();
+            let path = dir.path();
+            let repo = git2::Repository::init(path).unwrap();
+            repo.remote("origin", "file:///dev/null").unwrap();
+            let sig = repo.signature().unwrap();
+
+            let mut idx = repo.index().unwrap();
+            let id = idx.write_tree().unwrap();
+            let tree = repo.find_tree(id).unwrap();
+            let oid = repo
+                .commit(Some("HEAD"), &sig, &sig, "initial commit", &tree, &[])
+                .unwrap();
+
+            repo.reference("refs/remotes/origin/master", oid, true, "remote")
+                .unwrap();
+            let mut local_branch = repo.find_branch("master", git2::BranchType::Local).unwrap();
+            local_branch.set_upstream(Some("origin/master")).unwrap();
+
+            let commit_oid = repo.find_commit(oid).unwrap();
+            let new_oid = repo
+                .commit(
+                    Some("HEAD"),
+                    &sig,
+                    &sig,
+                    "second commit",
+                    &tree,
+                    &[&commit_oid],
+                )
+                .unwrap();
+
+            let oid_obj = repo.find_object(oid, None).unwrap();
+            repo.reset(&oid_obj, git2::ResetType::Hard, None).unwrap();
+            repo.reference("refs/remotes/origin/master", new_oid, true, "remote")
+                .unwrap();
+
+            repo.commit(
+                Some("HEAD"),
+                &sig,
+                &sig,
+                "third commit",
+                &tree,
+                &[&commit_oid],
+            )
+            .unwrap();
+
+            assert_eq!(Repo::Clean(Sync::Diverged), parse(path));
+            std::fs::write(path.join("yo"), b"yo").unwrap();
+            assert_eq!(Repo::Dirty(Sync::Diverged), parse(path));
+        }
+
+        #[test]
+        fn up_to_date() {
+            let dir = tempfile::tempdir().unwrap();
+            let path = dir.path();
+            let repo = git2::Repository::init(path).unwrap();
+            repo.remote("origin", "file:///dev/null").unwrap();
+            let sig = repo.signature().unwrap();
+
+            let mut idx = repo.index().unwrap();
+            let id = idx.write_tree().unwrap();
+            let tree = repo.find_tree(id).unwrap();
+            let oid = repo
+                .commit(Some("HEAD"), &sig, &sig, "initial commit", &tree, &[])
+                .unwrap();
+
+            repo.reference("refs/remotes/origin/master", oid, true, "remote")
+                .unwrap();
+            let mut local_branch = repo.find_branch("master", git2::BranchType::Local).unwrap();
+            local_branch.set_upstream(Some("origin/master")).unwrap();
+
+            assert_eq!(Repo::Clean(Sync::UpToDate), parse(path));
+            std::fs::write(path.join("yo"), b"yo").unwrap();
+            assert_eq!(Repo::Dirty(Sync::UpToDate), parse(path));
+        }
+
+        #[test]
+        fn local() {
+            let dir = tempfile::tempdir().unwrap();
+            let path = dir.path();
+            let repo = git2::Repository::init(path).unwrap();
+            let sig = repo.signature().unwrap();
+
+            let mut idx = repo.index().unwrap();
+            let id = idx.write_tree().unwrap();
+            let tree = repo.find_tree(id).unwrap();
+            repo.commit(Some("HEAD"), &sig, &sig, "initial commit", &tree, &[])
+                .unwrap();
+
+            assert_eq!(Repo::Clean(Sync::Local), parse(path));
+            std::fs::write(path.join("yo"), b"yo").unwrap();
+            assert_eq!(Repo::Dirty(Sync::Local), parse(path));
+        }
+    }
+
+    #[test]
+    fn pending() {
+        let dir = tempfile::tempdir().unwrap();
+        let path = dir.path();
+        let repo = git2::Repository::init(path).unwrap();
+        let sig = repo.signature().unwrap();
+
+        let mut idx = repo.index().unwrap();
+        let id = idx.write_tree().unwrap();
+        let tree = repo.find_tree(id).unwrap();
+        let oid = repo
+            .commit(Some("HEAD"), &sig, &sig, "initial commit", &tree, &[])
+            .unwrap();
+        let commit = repo.find_commit(oid).unwrap();
+
+        repo.cherrypick(&commit, None).unwrap();
+
+        assert_eq!(Repo::Pending, parse(path));
+    }
+
+    #[test]
+    fn detached() {
+        let dir = tempfile::tempdir().unwrap();
+        let path = dir.path();
+        let repo = git2::Repository::init(path).unwrap();
+        let sig = repo.signature().unwrap();
+
+        let mut idx = repo.index().unwrap();
+        let id = idx.write_tree().unwrap();
+        let tree = repo.find_tree(id).unwrap();
+        let oid = repo
+            .commit(Some("HEAD"), &sig, &sig, "initial commit", &tree, &[])
+            .unwrap();
+        let oid = repo.find_commit(oid).unwrap();
+
+        repo.commit(Some("HEAD"), &sig, &sig, "second commit", &tree, &[&oid])
+            .unwrap();
+
+        let target = repo.revparse_single("HEAD~1").unwrap();
+        repo.checkout_tree(&target, None).unwrap();
+        repo.set_head_detached(target.id()).unwrap();
+        assert_eq!(Repo::Detached, parse(path));
+    }
+
+    #[test]
+    fn untracked() {
+        let dir = tempfile::tempdir().unwrap();
+        let path = dir.path();
+        git2::Repository::init(path).unwrap();
+        assert_eq!(Repo::Untracked, parse(path));
+    }
 }
