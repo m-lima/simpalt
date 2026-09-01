2a73933ede032ef0b5bc66f6f45ef40d86d28359
Marcelo Lima <m-lima@users.noreply.github.com>
2026-09-01 15:19:33 +0200

    Giving it back to linux

--- src/bin/tmux/print/media.rs
+++ src/bin/tmux/print/media.rs
@@ -1,14 +1,30 @@
 use simpalt::print::{Color, Div, Printer, Symbol};
 
+const LENGTH: usize = 64;
+
 pub fn render<P>(printer: &mut P) -> crate::Result
 where
     P: Printer,
 {
-    let Some((playing, track)) = get_mpris_track() else {
-        return Ok(());
-    };
+    if let Some((playing, track)) = get_track() {
+        render_track(printer, playing, &track)
+    } else {
+        Ok(())
+    }
+}
 
-    render_track(printer, playing, &track)
+#[cfg(target_os = "macos")]
+fn get_track() -> Option<(bool, String)> {
+    let info = media_remote::get_info()?;
+    let playing = info.is_playing?;
+    let track = format!(
+        "{s_title} {title} {s_artist} {artist}",
+        s_title = Symbol::Song,
+        title = info.title?,
+        s_artist = Symbol::Artist,
+        artist = info.artist.unwrap_or_default()
+    );
+    Some((playing, track))
 }
 
 #[cfg(target_os = "linux")]
@@ -45,6 +61,7 @@ fn get_track() -> Option<(bool, String)> {
     paused.map(|t| (false, t))
 }
 
+#[cfg(target_os = "linux")]
 fn make_track_name(title: &str, artists: &[&str]) -> String {
     [Symbol::Song.str(), " ", title]
         .iter()
@@ -60,27 +77,28 @@ fn make_track_name(title: &str, artists: &[&str]) -> String {
         })
 }
 
+// Needs a pre-allocated String for track so that we can calculate the scroll
 fn render_track<P>(printer: &mut P, playing: bool, track: &str) -> crate::Result
 where
     P: Printer,
 {
-    let track = if track.len() > 64
-        && let Ok(len) = u64::try_from(2 * (track.len() - 64))
+    let track = if track.len() > LENGTH
+        && let Ok(len) = u64::try_from(2 * (track.len() - LENGTH))
         && let Ok(secs) = std::time::SystemTime::now()
             .duration_since(std::time::UNIX_EPOCH)
             .map(|d| d.as_secs() % len)
         && let Ok(start) = usize::try_from(secs)
     {
-        let start = if start < track.len() - 64 {
+        let start = if start < track.len() - LENGTH {
             start
         } else {
-            2 * (track.len() - 64) - start
+            2 * (track.len() - LENGTH) - start
         };
         let mut graphemes =
             unicode_segmentation::UnicodeSegmentation::grapheme_indices(track, true).skip(start);
 
         let fisrt_byte = graphemes.next().map_or(0, |(b, _)| b);
-        let last_byte = graphemes.nth(63).map_or(track.len(), |(b, _)| b);
+        let last_byte = graphemes.nth(LENGTH - 1).map_or(track.len(), |(b, _)| b);
 
         &track[fisrt_byte..last_byte]
     } else {
